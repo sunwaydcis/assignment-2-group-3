8dfa27156869ef1d503029886ceae3fa30e11c45
Humayra78 <22103535@imail.sunway.edu.my>
2024-11-18 11:37:25 +0800

    Wrote the first question

--- src/main/scala/assignment_2.scala
+++ src/main/scala/assignment_2.scala
@@ -2,3 +2,5 @@ import scala.io.Source
 
 val source = Source.fromFile("hospital.csv") //get the hospital data
 val lines = source.getLines.toSet //put it into preferred collection (rn its Set -> must change according to use and justify)
+
+//Which state has the highest total hospital bed ?
