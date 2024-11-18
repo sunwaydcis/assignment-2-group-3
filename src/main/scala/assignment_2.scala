5b3e842b04c07ed838c7e8e9903b5c8a94796142
atikvh <153811795+atikvh@users.noreply.github.com>
2024-11-18 11:27:25 +0800

    Importing the CSV file
    -> Using scala.io.Source

---
+++ src/main/scala/assignment_2.scala
@@ -0,0 +1,4 @@
+import scala.io.Source
+
+val source = Source.fromFile("hospital.csv") //get the hospital data
+val lines = source.getLines.toSet //put it into preferred collection (rn its Set -> must change according to use and justify)
